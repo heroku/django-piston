641748f33c2ca1dec28e0a51a0645c3361d87d72
Howard Burrows <howard@heroku.com>
2014-01-15 16:55:30 -0800

    Fixing syntax error

--- piston/resource.py
+++ piston/resource.py
@@ -306,7 +306,7 @@ class Resource(object):
             exc_type, exc_value, tb = sys.exc_info()
             rep = ExceptionReporter(request, exc_type, exc_value, tb.tb_next)
 
-            logger.error(format_error('\n'.join(rep.format_exception()))))
+            logger.error(format_error('\n'.join(rep.format_exception())))
 
             if self.email_errors:
                 self.email_exception(rep)
